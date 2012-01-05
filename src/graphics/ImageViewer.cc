3a571a4002f09364a4c9f6e159e0c62f73e09b07
ulyssi <ulyssi@free.fr>
2012-01-05 09:18:14 +0000

    debut gestion ouverture image [bug de non ouverture]

--- src/graphics/ImageViewer.cc
+++ src/graphics/ImageViewer.cc
@@ -1,14 +1,18 @@
 #include "ImageViewer.hh"
 
-#include "Picture.hh"
+#include "Utils.hh"
+#include "../model/Picture.hh"
+
 
 ImageViewer::ImageViewer(Picture* picture) {
   m_imageLabel = new QLabel;
-  m_imageLabel->setBackgroundRole(QPalette::Base);
-  m_imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
-  m_imageLabel->setScaledContents(true);
+  // m_imageLabel->setBackgroundRole(QPalette::Base);
+  // m_imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+  // m_imageLabel->setScaledContents(true);
+  
+  m_imageLabel->setPixmap(Utils::QPixmapFrom(picture));
 
-  setBackgroundRole(QPalette::Dark);
+  // setBackgroundRole(QPalette::Dark);
   setWidget(m_imageLabel);
 }
 

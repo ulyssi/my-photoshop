b0e73b3ea4bb765710f2044b8e0aa8ed37ff4e48
Johnnyarctocebus <Johnnyarctocebus@gmail.com>
2012-01-06 14:26:34 +0000

    pixelmod

---
+++ src/model/PixelMod.hh
@@ -0,0 +1,19 @@
+#ifndef _PIXELMOD_HH_
+#define _PIXELMOD_HH_
+
+#define ALPHA_MSK 0xFF000000
+#define RED_MSK 0x00FF0000
+#define GREEN_MSK 0x0000FF00
+#define BLUE_MSK 0x000000FF
+
+
+namespace PixelMod{
+
+  unsigned int combineAlpha(float,float);
+  unsigned int combineRed(unsigned int,unsigned int,float);
+  unsigned int combineBlue(unsigned int,unsigned int,float);
+  unsigned int combineGreen(unsigned int,unsigned int,float);
+  float getAlpha(unsigned int, float);
+
+};
+#endif

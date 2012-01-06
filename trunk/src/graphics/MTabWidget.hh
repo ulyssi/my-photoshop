a5aaadb26cb75c98cef4156140bfde2b4c123d78
ulyssi@free.fr <ulyssi@free.fr@45933bd7-4c1c-61c1-bc9f-14f6a39d67b9>
2012-01-06 17:14:22 +0000

    pas parfait

--- trunk/src/graphics/MTabWidget.hh
+++ trunk/src/graphics/MTabWidget.hh
@@ -4,21 +4,28 @@
 #include <QTabWidget>
 #include <QTabBar>
 #include <QWidget>
-
+#include <QPushButton>
 
 class MTabWidget : public QTabWidget {Q_OBJECT
 public :
 
   MTabWidget (QWidget * parent = 0 );
   ~MTabWidget ();
-  //override private methode from QTabWidget
-  QTabBar* tabBar();
   void fermerOnglet(int);
+  /** Override public Methode */
+ 
+  int addTab ( QWidget * page, const QString & label );
+  int addTab ( QWidget * page, const QIcon & icon, const QString & label );
+
+  /**override private methode from QTabWidget */
+  QTabBar* tabBar();
+ 
 public slots :
-
+  void createAddTab();
   void myItemChangeSlot(int index);
-
+  QPushButton* createCloseButton();
 private :
+  QPushButton* m_pushButton; 
   
   
 

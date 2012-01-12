6f154bac3e4c3eda7b44897b8bbf0394489711e2
ulyssi@free.fr <ulyssi@free.fr@45933bd7-4c1c-61c1-bc9f-14f6a39d67b9>
2012-01-12 17:14:22 +0000

    reecriture de code sur PictureManager.cc a faire

--- trunk/src/graphics/picture/PictureManager.cc
+++ trunk/src/graphics/picture/PictureManager.cc
@@ -2,6 +2,7 @@
 #include "UserInterface.hh"
 #include "PictureButton.hh"
 #include "PictureModifier.hh"
+#include "Picture.hh"
 #include "math.h"
 #include <iostream>
 /** Constructeurs et destructeur */
@@ -16,6 +17,8 @@ PictureManager::PictureManager(UserInterface* userInterface) :
   m_layout= new QGridLayout();
   m_layout->setAlignment(Qt::AlignCenter);
   m_layout->setSpacing(10);
+  m_QWidget->setLayout(m_layout);
+  setWidget(m_QWidget);
 }
 
 PictureManager::~PictureManager() {
@@ -31,21 +34,29 @@ void PictureManager::addPictureModifier(PictureModifier* pictureModifier) {
   refresh();
 }
 
-void PictureManager::removePictureModifier(PictureModifier* picturemodifier){
-
+void PictureManager::removePictureModifier(PictureModifier* picturemodifier){ 
+  int r;
+  for (int i =0 ; i<m_buttonGroup->buttons().size();i++){
+    if(((PictureButton*)m_buttonGroup->buttons().at(i))->getPicture()==picturemodifier->getPicture()){
+      r=i;
+    }
+  }
+  m_layout->removeWidget(m_buttonGroup->buttons().at(r));
+  m_buttonGroup->buttons().at(r)->hide();
+  m_QWidget->repaint(0,0,-1,-1);
+  m_buttonGroup->removeButton(m_buttonGroup->buttons().at(r));
+  refresh();
 }
 
 
 void PictureManager::refresh() {
- 
   if (parentWidget()!=NULL&&m_buttonGroup->buttons().size()!=0){
-    
     int t_width =parentWidget()->size().width();
     QSize* b_size =((PictureButton*)m_buttonGroup->buttons().first())->geticonSize();
     m_nbCol=floor((t_width-m_layout->spacing())/(b_size->width()+m_layout->spacing())+0.5);    
     m_nbLig=ceil(float(m_buttonGroup->buttons().size())/float(m_nbCol));
-    if (m_nbCol==0) m_nbCol=1;
-    if (m_nbLig==0) m_nbLig=1;
+    if (m_nbCol<=1) m_nbCol=2;
+    if (m_nbLig<=1) m_nbLig=2;
     m_QWidget->resize(t_width-20,m_nbLig*b_size->height()+m_layout->spacing()*(m_nbLig*2));
     for (int i =0 ; i<m_buttonGroup->buttons().size();i++)
       m_layout->removeWidget((QPushButton*)m_buttonGroup->buttons().at(i));
@@ -58,9 +69,10 @@ void PictureManager::refresh() {
      	l++;
       }
     }
-    m_QWidget->setLayout(m_layout);
-    setWidget(m_QWidget);
+   
+   
   }
+  
 }
 
 /** Override public method from QWidget **/

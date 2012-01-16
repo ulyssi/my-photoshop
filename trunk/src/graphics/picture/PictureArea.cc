609b329d4ee9fcc2378201c8aa787d022fd3dbd4
ulyssi@free.fr <ulyssi@free.fr@45933bd7-4c1c-61c1-bc9f-14f6a39d67b9>
2012-01-16 16:23:23 +0000

    conversion en graphview /graphScene

--- trunk/src/graphics/picture/PictureArea.cc
+++ trunk/src/graphics/picture/PictureArea.cc
@@ -1,31 +1,23 @@
 #include "PictureArea.hh"
+#include "PictureModifier.hh"
+#include "PictureViewer.hh"
 
-
-PictureArea::PictureArea(PictureModifier* p):
-  PictureViewer(p){
-  m_rubberBand= new MRubberBand(QRubberBand::Rectangle,this);
+PictureArea::PictureArea(PictureModifier* p){
+  m_Scene=new QGraphicsScene(this);
+  m_Scene->setBackgroundBrush(QColor(255, 255, 255, 0));
+  m_Scene->setForegroundBrush(QColor(255, 255, 255, 0));
+  setScene(m_Scene);
+  m_pictureViewer = new PictureViewer(p) ;
+  m_proxy = m_Scene->addWidget(m_pictureViewer);
+  show();
 }
 
 PictureArea::~PictureArea(){}
 
-
-
-#include  <iostream>
-void PictureArea::mousePressEvent(QMouseEvent *event){
-   m_origin = event->pos();
-     if (!m_rubberBand)
-         m_rubberBand = new MRubberBand(QRubberBand::Rectangle, this);
-     m_rubberBand->setGeometry(QRect(m_origin, QSize()));
-     m_rubberBand->show();
-
-}
-
-void PictureArea::mouseMoveEvent(QMouseEvent *event){
-  m_rubberBand->setGeometry(QRect(m_origin, event->pos()).normalized());
-}
-
-void PictureArea::mouseReleaseEvent(QMouseEvent *event){
- 
-     // determine selection, for example using QRect::intersects()
-     // and QRect::contains().
- }
+void PictureArea::refresh(){
+  m_pictureViewer->refresh();
+  this->repaint();
+  QSize size= m_pictureViewer->sizeHint();
+  setSceneRect ( (qreal) 0, (qreal) 0, (qreal) size.width(), (qreal) size.height() );
+  adjustSize();
+} 
--- trunk/src/graphics/picture/PictureArea.hh
+++ trunk/src/graphics/picture/PictureArea.hh
@@ -1,27 +1,25 @@
 #ifndef _PICTURE_AREA_HH
 #define _PICTURE_AREA_HH
 
-#include <QRubberBand>
-#include <QMouseEvent>
-#include <QGraphicsScene>
-class PictureModifier;
-#include "MRubberBand.hh"
-#include "PictureViewer.hh"
 
-class PictureArea: public PictureViewer{ Q_OBJECT
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsProxyWidget>
+class PictureModifier;
+class PictureViewer;
+
+
+class PictureArea: public QGraphicsView { Q_OBJECT
 public: 
   PictureArea(PictureModifier*);
   ~PictureArea();
-  /** Override from Qwidget**/
-  void  mousePressEvent(QMouseEvent *event);
-  void  mouseMoveEvent(QMouseEvent *event);
-  void  mouseReleaseEvent(QMouseEvent *event);
-
-/** attributs **/
-  MRubberBand* m_rubberBand;
-  QPoint m_origin;
-
-
+  void refresh();
+private:
+  /**Attributes**/
+  QGraphicsScene *m_Scene;
+  PictureViewer* m_pictureViewer;
+  QGraphicsRectItem* m_selectionTool;
+  QGraphicsProxyWidget* m_proxy;
 };
 
 #endif
